5606ef9bd02eba098c9c55ac9e4cac1967841260
phoebe-ch <89631015+phoebe-ch@users.noreply.github.com>
2022-12-04 14:14:06 -0800

    minor improvements to Day 4 code

--- Day4.py
+++ Day4.py
@@ -14,19 +14,11 @@ counter = 0
 with open(file) as f:
     lines = f.readlines()
     for row in lines:
-        splitElf = row.split(",")
-        firstElf = splitElf[0]
-        secondElf = splitElf[1]
+        firstElf,secondElf = row.split(",")
 
-        splitFirst = firstElf.split("-")
-        splitSecond = secondElf.split("-")
-
-        firstStart = int(splitFirst[0])
-        firstEnd = int(splitFirst[1])
-        
-        secondStart = int(splitSecond[0])
-        secondEnd = int(splitSecond[1])
-
+        firstStart,firstEnd = [int(x) for x in firstElf.split("-")]
+        secondStart,secondEnd = [int(x) for x in secondElf.split("-")]
+    
         firstRange = np.linspace(firstStart,firstEnd,firstEnd-firstStart+1)
         secondRange = np.linspace(secondStart,secondEnd,secondEnd-secondStart+1)
 
@@ -58,19 +50,11 @@ counter = 0
 with open(file) as f:
     lines = f.readlines()
     for row in lines:
-        splitElf = row.split(",")
-        firstElf = splitElf[0]
-        secondElf = splitElf[1]
+        firstElf,secondElf = row.split(",")
 
-        splitFirst = firstElf.split("-")
-        splitSecond = secondElf.split("-")
-
-        firstStart = int(splitFirst[0])
-        firstEnd = int(splitFirst[1])
-        
-        secondStart = int(splitSecond[0])
-        secondEnd = int(splitSecond[1])
-
+        firstStart,firstEnd = [int(x) for x in firstElf.split("-")]
+        secondStart,secondEnd = [int(x) for x in secondElf.split("-")]
+    
         firstRange = np.linspace(firstStart,firstEnd,firstEnd-firstStart+1)
         secondRange = np.linspace(secondStart,secondEnd,secondEnd-secondStart+1)
 
