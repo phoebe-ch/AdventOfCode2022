7bcd8afbb3dc54ff7a01c5c19cdf6be672f6093d
phoebe-ch <89631015+phoebe-ch@users.noreply.github.com>
2022-12-03 16:29:01 -0800

    Completed Day 3

---
+++ Day3.py
@@ -0,0 +1,55 @@
+"""
+AoC 2022, Day 3
+
+Phoebe Cheung
+Dec 3, 2022
+"""
+# %% Part 1
+
+file = "Day3_Input.txt"
+
+totalScore = 0
+
+with open(file) as f:
+    lines = f.readlines()
+
+    for row in lines:
+        firstHalf = row[:len(row)//2]
+        secondHalf = row[len(row)//2:]
+
+        commonChar = ''.join(set(firstHalf).intersection(secondHalf))
+        ascii = ord(commonChar)
+        
+        if ascii < 91: # capital letters
+            score = ascii - 38
+        else:
+            score = ascii - 96
+
+        totalScore = totalScore + score
+
+print(totalScore)
+
+# %% Part 2
+import numpy as np
+
+file = "Day3_Input.txt"
+
+totalScore = 0
+
+with open(file) as f:
+    lines = f.readlines()
+    
+    i = 0
+    while i < len(lines):
+        commonChar = ''.join(set(lines[i]).intersection(lines[i+1],lines[i+2]))
+        ascii = ord(max(commonChar))
+        if ascii < 91: # capital letters
+            score = ascii - 38
+        else:
+            score = ascii - 96
+
+        totalScore = totalScore + score
+
+        i = i + 3
+
+print(totalScore)
